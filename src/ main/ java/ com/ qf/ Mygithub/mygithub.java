e1372d2c605c63b1f3d7194cfec01da0c90f717e
CHENLU-2019 <56904833+CHENLU-2019@users.noreply.github.com>
2019-10-23 20:00:54 +0800

    Update mygithub.java

--- src/ main/ java/ com/ qf/ Mygithub/mygithub.java
+++ src/ main/ java/ com/ qf/ Mygithub/mygithub.java
@@ -6,6 +6,6 @@ package com.qf.Mygithub;
  */
 public class mygithub {
     public static void main(String[] args) {
-        System.out.println("从idea提交文件到GitHub");asdf
+        System.out.println("从idea提ssafsd交文件到GitHubfgsdfg");
     }
 }
