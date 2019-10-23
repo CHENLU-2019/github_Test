d72e69c8aaa2d16bd7471122c979f7520716f057
CHENLU <1215597420@qq.com>
2019-10-23 20:01:55 +0800

    第二次测试

--- src/ main/ java/ com/ qf/ Mygithub/mygithub.java
+++ src/ main/ java/ com/ qf/ Mygithub/mygithub.java
@@ -6,6 +6,7 @@ package com.qf.Mygithub;
  */
 public class mygithub {
     public static void main(String[] args) {
+        System.out.println("从idea提交文件到GitHubasdf");
         System.out.println("从idea提ssafsd交文件到GitHubfgsdfg");
     }
 }
