69d2e2b690cf14bd52c0930e827803739301293a
CHENLU <1215597420@qq.com>
2019-10-23 19:29:20 +0800

    第一次从idea提交文件到GitHub

---
+++ src/main/java/com/qf/Mygithub/mygithub.java
@@ -0,0 +1,11 @@
+package com.qf.Mygithub;
+
+/**
+ * @author CL
+ * @Date 2019/10/23
+ */
+public class mygithub {
+    public static void main(String[] args) {
+        System.out.println("从idea提交文件到GitHub");
+    }
+}
